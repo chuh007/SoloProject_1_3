22ce43714ba545fb78607751e0537802e2c8f58c
chuh007 <chhuh007@gmail.com>
2024-11-20 19:46:16 +0900

    Bullet

--- SoloProject_1_3/Assets/01.Scripts/Core/GameManager.cs
+++ SoloProject_1_3/Assets/01.Scripts/Core/GameManager.cs
@@ -6,10 +6,23 @@ public class GameManager : MonoSingleton<GameManager>
 {
     private Dictionary<string, Pool> _pools;
 
-    [SerializeField] private GameObject _bullet;
+    [SerializeField] private BulletDice _bullet;
     private void Awake()
     {
         _pools = new Dictionary<string, Pool>();
-        // Pool bulletPool = new Pool(_bullet, transform, 15);
+        Pool bulletPool = new Pool(_bullet, transform, 15);
+        _pools.Add(_bullet.PoolName, bulletPool);
+    }
+
+    public IPoolable Pop(string poolName)
+    {
+        IPoolable item = _pools[poolName].Pop();
+        item.ResetItem();
+        return item;
+    }
+
+    public void Push(IPoolable item)
+    {
+        _pools[item.PoolName].Push(item);
     }
 }
--- SoloProject_1_3/Assets/01.Scripts/Player/PlayerAttackCompo.cs
+++ SoloProject_1_3/Assets/01.Scripts/Player/PlayerAttackCompo.cs
@@ -1,9 +1,13 @@
 using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerAttackCompo : MonoBehaviour, IEntityComponent
 {
     [SerializeField] private float _atkCooldown;
+    [SerializeField] private int _damage;
+    [SerializeField] private BulletDice _dicePrefab;
+    [SerializeField] private Transform ShootPoint;
 
     private Player _player;
     private float _lastAtkTime;
@@ -24,5 +28,15 @@ public class PlayerAttackCompo : MonoBehaviour, IEntityComponent
     private void Attack()
     {
         Debug.Log("공격이다임마");
+        Projectile newBullet = GameManager.Instance.Pop("BulletDice") as Projectile;
+        newBullet.InitAndFire(ShootPoint, _damage);
+    }
+    private void FixedUpdate()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 point = Camera.main.ScreenToWorldPoint(mousePos);
+        Vector2 dir = point - transform.position;
+        float angle = MathF.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.localRotation = Quaternion.Euler(0,0,angle);
     }
 }
